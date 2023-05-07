74df54bacb50bb962ac3d50a8e976dac4e847401
Chin-Ho Lin <tainvecs@gmail.com>
2023-05-07 23:35:21 +0800

    feat: add server models

---
+++ src/servers/models/picture.py
@@ -0,0 +1,30 @@
+import datetime
+import uuid
+
+
+class Picture:
+
+    """model for saving picture on s3"""
+
+    def __init__(
+            self,
+            uuid_str:   str = None,
+            created_at: datetime.datetime = None,
+            filename:   str = None,
+            size:       str = None,
+            url:        str = None
+    ):
+
+        time_now        = datetime.datetime.now()
+        timestamp_str   = time_now.strftime("%Y%m%d-%s")
+
+        self.uuid       = uuid_str or str(uuid.uuid4())
+        self.created_at = created_at or time_now
+        self.filename   = filename or f'{timestamp_str}.{self.uuid}.png'
+        self.size       = size
+        self.url        = url
+
+        self.meta_data = {
+            "ref_id":   None,
+            "ref_type": None,  # 'mood_pic'
+        }
