7751279f392827b85f69ce1fdc66d435afcd84ec
Chin-Ho Lin <tainvecs@gmail.com>
2023-09-11 15:15:28 +0800

    fix: change format of spot source

--- src/servers/logics/spot.py
+++ src/servers/logics/spot.py
@@ -17,18 +17,18 @@ _GLEN_TIMEOUT = 60.0
 _GMAP_TIMEOUT = 10.0
 
 _SPOT_SOURCE_WHITELIST_SET = {
-    'agoda.com',
-    'dreamstime.com',
-    'expedia.com',
-    'istockphoto.com',
-    'kiwicollection.comtrip.com',
-    'kkday.com',
-    'klook.com',
-    'tripadvisor.com'
+    'agoda',
+    'dreamstime',
+    'expedia',
+    'istockphoto',
+    'kiwicollection',
+    'kkday',
+    'klook',
+    'tripadvisor'
 }
 
 _SPOT_SOURCE_BLACKLIST_SET = {
-    'skyscrapercity.com'
+    'skyscrapercity'
 }
 
 
@@ -82,9 +82,16 @@ def search_spot_image_by_pic_url(api_key: str, pic_url: str) -> list[spot_models
     prioritized_matches = []
     other_matches = []
     for v_match in glen_visual_matches:
-        if v_match['source'] in _SPOT_SOURCE_WHITELIST_SET:
-            prioritized_matches.append(v_match)
-        else:
+
+        pass_bool = False
+        for v_src in _SPOT_SOURCE_WHITELIST_SET:
+            if v_src in v_match['source'].lower():
+                prioritized_matches.append(v_match)
+                pass_bool = True
+                print(v_match)
+                break
+
+        if not pass_bool:
             other_matches.append(v_match)
 
     filtered_matches = prioritized_matches
